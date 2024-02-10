27b7adb6bd19504116215f663468efc21ff73110
adrianoferreira-dev <adrianoferreira.dev@gmail.com>
2024-02-10 17:19:03 -0300

    hey-25: it should prune a question archive more than a month

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,16 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
-use Illuminate\Database\Eloquent\{Model, SoftDeletes};
+use Illuminate\Database\Eloquent\{Builder, Model, Prunable, SoftDeletes};
 
 class Question extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Prunable;
 
     protected $casts = [
         'draft' => 'bool',
     ];
+
+    public function prunable(): Builder
+    {
+        return static::where('deleted_at', '<=', now()->subMonth());
+    }
 
     public function createdBy(): BelongsTo
     {
