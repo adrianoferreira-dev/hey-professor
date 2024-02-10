453c6573bffa3cc251dd6fb77e4ad3994a6055ee
adrianoferreira-dev <adrianoferreira.dev@gmail.com>
2024-02-10 12:28:38 -0300

    hey-19: it should make sure that only question with status DRAFT and only if the question wsa creaated by the loggend user can be updated

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -39,6 +39,8 @@ class QuestionController extends Controller
 
     public function update(Question $question): RedirectResponse
     {
+        $this->authorize('update', $question);
+
         $question->question = request()->question;
         $question->save();
 
--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -19,3 +19,37 @@ it('should de able to update a question', function () {
     expect($question)
         ->question->toBe('Updated Question?');
 });
+
+it('should make sure that only question with status DRAFT can be updated', function () {
+    $user             = User::factory()->create();
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+    $draftQuestion    = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    $data = ['question' => 'Updated Question?'];
+
+    put(route('question.update', $questionNotDraft), $data)
+        ->assertForbidden();
+
+    put(route('question.update', $draftQuestion), $data)
+        ->assertRedirect();
+});
+
+it('should make sure that only the person who has created the question can update the question', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+
+    $data = ['question' => 'Updated Question?'];
+
+    put(route('question.update', $question), $data)
+        ->assertForbidden();
+
+    actingAs($rightUser);
+
+    put(route('question.update', $question), $data)
+        ->assertRedirect();
+});
